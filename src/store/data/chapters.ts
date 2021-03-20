ee708237eec60f401ffee3efec23d1e8857dc5cc
Marc <marc@mrdeliver.de>
2021-03-20 13:23:32 +0100

    add tipps and glossar entries

--- src/store/data/chapters.ts
+++ src/store/data/chapters.ts
@@ -12,13 +12,22 @@ export default {
           heading: '',
           text: 'Die Terrorherrschaft, die Schreckensherrschaft oder der Schrecken (französisch La Terreur, „Der Schrecken“) war eine Periode der Französischen Revolution von Anfang Juni 1793 bis Ende Juli 1794, die durch die brutale Unterdrückung aller Personen gekennzeichnet war, die verdächtigt wurden, Gegner der Revolution zu sein. Die Terrorherrschaft wurde vom Wohlfahrtsausschuss, einem Komitee von zwölf Männern, geleitet. Zuerst wurde es von Georges Danton und dann zunehmend von Maximilien de Robespierre angeführt.',
           glossarEntries: ['1'],
-          tipps: [],
+          tipps: [
+            {
+              heading: 'Frankreich',
+              text: 'Frankreich im Westen Europas ist ein Land mit mittelalterlichen Städten, Bergdörfern und Mittelmeerstränden. Die Hauptstadt Paris gilt als erstrangiges Mode- und Kulturzentrum.'
+            },
+            {
+              heading: 'Europa',
+              text: 'Europa ist ein Erdteil, der sich über das westliche Fünftel der eurasischen Landmasse erstreckt. Obwohl es geographisch gesehen nur ein Subkontinent ist, der mit Asien zusammen den Kontinent Eurasien bildet, wird es historisch und kulturell begründet oft als eigenständiger Kontinent betrachtet.'
+            },
+          ],
         },
         {
           type: ContentBlockType.TEXT_CONTENT_BLOCK,
           heading: 'Wie alles begann...',
           text: 'Die Terrorherrschaft begann mit dem Aufstand der Pariser Sansculotten gegen den Konvent vom 31. Mai bis 2. Juni 1793 und der Annahme der Verfassung vom 24. Juni 1793, die allerdings nie in Kraft trat. Ihren Höhepunkt erreichte sie im Juni und Juli 1794; diese Zeit wird auch als Der Große Terror (frz. la Grande Terreur) bezeichnet. Sie fand ein Ende mit der Verhaftung und Hinrichtung Robespierres und der Machtübernahme der Thermidorianer am 9. Thermidor II (27. Juli 1794).',
-          glossarEntries: [],
+          glossarEntries: [2],
           tipps: [],
         },
       ],
--- src/store/data/glossar.ts
+++ src/store/data/glossar.ts
@@ -7,6 +7,12 @@ export default {
         text: 'Maximilien de Robespierre ([maksimiˈljɛ̃ də ʀɔbɛsˈpjɛːʀ]; * 6. Mai 1758 in Arras; † 28. Juli 1794 in Paris; getauft Maximilien-François-Marie-Isidore), häufig nur Maximilien Robespierre[1], auch „der Unbestechliche“ genannt, war ein französischer Rechtsanwalt, Revolutionär und führender Politiker der Jakobiner. Er wirkte ab 1789 auf die erste Phase der Französischen Revolution ein und gewann bis kurz vor seiner Hinrichtung 1794 einen zunehmend prägenden Einfluss auf ihre Entwicklung. Nach dem Beginn des Ersten Koalitionskriegs war er auf der innenpolitischen Ebene der 1792 ausgerufenen ersten Französischen Republik einer der maßgeblichen Initiatoren für die als „Verteidigung der Republik“ begründete Schreckensherrschaft (frz. la Terreur) von 1793/94.',
         imageUrl: 'https://de.wikipedia.org/wiki/Datei:Robespierre.jpg',
       },
+      {
+        id: '2',
+        heading: 'Sansculottes',
+        text: 'Als Sansculottes wurden in der Zeit der Französischen Revolution die Pariser Arbeiter und Kleinbürger bezeichnet, die im Gegensatz zu den von Adligen getragenen Kniebundhosen oftmals lange Hosen trugen. ',
+        imageUrl: '',
+      },
     ],
   },
 
