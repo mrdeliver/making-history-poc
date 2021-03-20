4aec511f12f97ef0a660c33d7e4605fd37272e77
simonstadlinger <stadlinger.simon@gmail.com>
2021-03-20 18:26:52 +0100

    Tip area changes

---
+++ src/components/content/tipp-area.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div v-for="(tipp, idx) in tipps" class="tipp-container" :key="idx">tipp</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Tip } from '@/store/data/data-types';
+import { Vue, Options } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Options({
+  name: 'tipp-area',
+})
+export default class TippArea extends Vue {
+  @Prop({})
+  private tipps: Tip[] = []
+
+  mounted(): void {
+    console.log('this is a tip');
+    console.log(`tipp area: ${this.tipps}`);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.tipp-container {
+  margin: 0 auto;
+  width: 30%;
+  margin-bottom: 20px;
+}
+
+</style>
