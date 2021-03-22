bc6cffc80fdb154a5329fae6c9dfcb43b8b3f789
simonstadlinger <stadlinger.simon@gmail.com>
2021-03-22 15:28:00 +0100

    save state

--- src/components/navigation/source-slider.vue
+++ src/components/navigation/source-slider.vue
@@ -1,36 +1,50 @@
 <template>
-  <router-link to="1">soure1</router-link>
-  <router-link to="2">source2</router-link>
+  <div class="page-slider-container">
+    <Slider :allLinks="sourceLinks"></Slider>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
 
-@Options({})
+import { Ressources } from '@/store/data/data-types';
+import PageStore, { Page } from '../../store/page-module';
+import Slider from '../slider/slider.vue';
+import SliderLink from '../slider/slider';
+
+@Options({
+  components: {
+    Slider,
+  },
+})
 export default class SourceSlider extends Vue {
   @Prop({ type: String })
-  private sourceId = ''
+  private sourceType = '';
 
   @Prop({ type: String })
-  private pageId = ''
+  private ressourceId = '';
+
+  private allSources: Ressources[] = []
+
+  private sliderLinks: SliderLink [] = []
+
+  created(): void {
+    this.allSources = this.pageSliderLinks = this.mapPageToSliderLink(this.allPages);
+  }
+
+  mapPageToSliderLink(pages: Page[]): SliderLink[] {
+    // primary --> link = page/pageId | content = name
+    return pages.map((page):SliderLink => ({
+      primaryLink: {
+        link: `/band/${this.bandId}/page/${page.id}`,
+        content: page.heading,
+      },
+    }));
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss">
 </style>
