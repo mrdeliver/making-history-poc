b495e8c0f8428d57060eb7a14dbbb63de344d87c
Moritz Spranger <moritz.spranger@gmail.com>
2021-07-04 10:45:19 +0200

    Style upload zone

--- src/components/structure/fileupload.vue
+++ src/components/structure/fileupload.vue
@@ -2,15 +2,21 @@
 
 <!-- HTML Template -->
 <template>
-  <div class="dropbox">
-    <input type="file" multiple :name="uploadFieldName" accept="image/*"
-    class="input-file" @change="updateFiles($event.target.files)">
-    <div class="heading">
-      Files hier ablegen oder klicken!
+  <div class="row block">
+    <div class="side-col"></div>
+    <div class="center-col">
+      <div class="dropbox">
+        <input type="file" multiple :name="uploadFieldName" accept="image/*"
+        class="input-file" @change="updateFiles($event.target.files)">
+        <div class="heading">
+          Files hier ablegen oder klicken!
+        </div>
+        <div v-for="file in files" :key="file">
+          {{file}}
+        </div>
+      </div>
     </div>
-    <div v-for="file in files" :key="file">
-      {{file}}
-    </div>
+    <div class="side-col"></div>
   </div>
 </template>
 
@@ -18,9 +24,9 @@
 import { Vue } from 'vue-class-component';
 
 export default class FileUpload extends Vue {
-  files: [] = [];
+  files: Array<string> = [];
 
-  updateFiles(filelist: []): void {
+  updateFiles(filelist: Array<HTMLInputElement>): void {
     filelist.forEach((file) => {
       this.files.push(file.name);
     });
@@ -30,15 +36,18 @@ export default class FileUpload extends Vue {
 
 <!-- SASS styling -->
 <style lang="scss">
+@import "src/colors";
+@import "src/size";
+
   .dropbox {
-    outline: 2px dashed grey; /* the dash box */
-    outline-offset: -10px;
-    background: lightcyan;
+    background: $color_green_1;
     color: dimgray;
     padding: 10px 10px;
     min-height: 200px; /* minimum height */
     position: relative;
     cursor: pointer;
+    border-radius: 15px;
+    border: 3px dashed $color_green_3;
   }
 
   .input-file {
@@ -52,4 +61,20 @@ export default class FileUpload extends Vue {
   .dropbox:hover {
     background: lightblue; /* when mouse over to the drop zone, change color */
   }
+
+  .side-col {
+    flex: $side_col_width
+  }
+
+  .center-col {
+    flex: $center_col_width
+  }
+
+  .row {
+    display: $row_display;
+  }
+
+  .block {
+  margin-bottom: 20px;
+}
 </style>
