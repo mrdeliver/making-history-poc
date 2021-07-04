f0a9be39d57490e4f717996b2d208c9746ba6c50
Marc <marc@mrdeliver.de>
2021-07-04 11:02:56 +0200

    First working version of image gallery

--- src/components/content/ressources/inline-multi-image-ressource.vue
+++ src/components/content/ressources/inline-multi-image-ressource.vue
@@ -3,26 +3,39 @@
     <div class='row'>
       <div class="image-col">
         <div>
-          <vue-chocolat :options="{ loop: true }">
-            <a
-              href="https://media.giphy.com/media/apRvTFYSP5oAw/giphy.gif"
-              title="Suspicious pug"
-            >
-              <img
-                src="https://media.giphy.com/media/apRvTFYSP5oAw/giphy.gif"
-                alt="Suspicious pug"
-              />
-            </a>
-            <a
-              href="https://media.giphy.com/media/1d7F9xyq6j7C1ojbC5/giphy.gif"
-              title="Dog smiling"
-            >
-              <img
-                src="https://media.giphy.com/media/1d7F9xyq6j7C1ojbC5/giphy.gif"
-                alt="Dog smiling"
-              />
-            </a>
-          </vue-chocolat>
+          <!-- Slideshow container -->
+          <div class="slideshow-container">
+
+            <!-- Full-width images with number and caption text -->
+            <div class="mySlides fade">
+              <div class="numbertext">1 / 3</div>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Franz%C3%B6sischer-friedhof-berlin_-_5.jpeg/640px-Franz%C3%B6sischer-friedhof-berlin_-_5.jpeg" style="width:100%">
+              <div class="text">Caption Text</div>
+            </div>
+
+            <div class="mySlides fade">
+              <div class="numbertext">2 / 3</div>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Revolutionary_War_artillery_on_display_at_Yorktown_Battlefield_image_11.jpg/800px-Revolutionary_War_artillery_on_display_at_Yorktown_Battlefield_image_11.jpg" style="width:100%">
+              <div class="text">Caption Two</div>
+            </div>
+
+            <div class="mySlides fade">
+              <div class="numbertext">3 / 3</div>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Franz%C3%B6sischer_Friedhof_Berlin-Mitte_Okt.2016_-_4.jpg/640px-Franz%C3%B6sischer_Friedhof_Berlin-Mitte_Okt.2016_-_4.jpg" style="width:100%">
+              <div class="text">Caption Three</div>
+            </div>
+
+            <!-- Next and previous buttons -->
+            <a class="prev" @click="plusSlides(-1)">&#10094;</a>
+            <a class="next" @click="plusSlides(1)">&#10095;</a>
+          </div>
+
+          <!-- The dots/circles -->
+          <div style="text-align:center">
+            <span class="dot" @click="currentSlide(1)"></span>
+            <span class="dot" @click="currentSlide(2)"></span>
+            <span class="dot" @click="currentSlide(3)"></span>
+          </div>
         </div>
       </div>
     </div>
@@ -50,19 +63,19 @@ import TextBlock from '../text-block.vue';
   },
 })
 export default class InlineMultiImageRessource extends Vue {
-  items = [
-    {
-      src: 'https://media.giphy.com/media/IrFqA9fFseZaM/giphy.gif',
-      title: 'Smart cow',
-    },
-    {
-      src: 'https://media.giphy.com/media/bympeqWadSL3G/giphy.gif',
-      title: 'Friendly cow',
-    },
-    {
-      src: 'https://media.giphy.com/media/LilPRDHg9Qre0/giphy.gif',
-    },
-  ]
+  // items = [
+  //   {
+  //     src: 'https://media.giphy.com/media/IrFqA9fFseZaM/giphy.gif',
+  //     title: 'Smart cow',
+  //   },
+  //   {
+  //     src: 'https://media.giphy.com/media/bympeqWadSL3G/giphy.gif',
+  //     title: 'Friendly cow',
+  //   },
+  //   {
+  //     src: 'https://media.giphy.com/media/LilPRDHg9Qre0/giphy.gif',
+  //   },
+  // ]
 
   @Prop()
   id = '';
@@ -84,6 +97,40 @@ export default class InlineMultiImageRessource extends Vue {
 
   @Prop()
   annotations: Annotation[] = [];
+
+  slideIndex = 1;
+
+  mounted(): void {
+    this.showSlides(this.slideIndex);
+  }
+
+  // Next/previous controls
+  plusSlides(n: number): void {
+    this.showSlides(this.slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  currentSlide(n: number): void {
+    this.showSlides(this.slideIndex = n);
+  }
+
+  showSlides(n: number):void {
+    let i;
+    const slides = document.getElementsByClassName('mySlides');
+    const dots = document.getElementsByClassName('dot');
+    if (n > slides.length) { this.slideIndex = 1; }
+    if (n < 1) { this.slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i += 1) {
+      const slide = slides[i] as HTMLElement;
+      slide.style.display = 'none';
+    }
+    for (i = 0; i < dots.length; i += 1) {
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+    const blockSlide = slides[this.slideIndex - 1] as HTMLElement;
+    blockSlide.style.display = 'block';
+    dots[this.slideIndex - 1].className += ' active';
+  }
 }
 
 </script>
@@ -91,29 +138,102 @@ export default class InlineMultiImageRessource extends Vue {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "src/colors";
-.block {
-  margin-bottom: 20px;
-}
-
-.text-block {
-  text-align: justify;
-}
-
-.image-col {
-  flex: 80%;
-  padding: 20px;
-}
-
-.caption-col {
-  width: 80%;
-  color: $color_yellow_7;
-  text-align: center;
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
   margin: auto;
+  padding: 20px;
+  padding-top: 0px;
+  padding-bottom: 0px;
 }
 
-img {
-  max-width:100%;
-  max-height:100%;
+/* Hide the images by default */
+.mySlides {
+  display: none;
+  img {
+    border-radius: 15px;
+  }
+}
+
+/* Next & previous buttons */
+.prev, .next {
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  margin-top: -22px;
+  padding: 16px;
+  color: white;
+  font-weight: bold;
+  font-size: 18px;
+  transition: 0.6s ease;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 20px;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover, .next:hover {
+  background-color: rgba(0,0,0,0.8);
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  //padding: 8px 12px;
+  position: absolute;
+  bottom: 20px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  cursor: pointer;
+  height: 10px;
+  width: 50px;
+  margin: 0 5px;
+  background-color: $color_yellow_6;
   border-radius: 15px;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active, .dot:hover {
+  background-color: $color_yellow_8;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4}
+  to {opacity: 1}
 }
 </style>
