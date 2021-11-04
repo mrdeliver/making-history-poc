9a5a73e774415782107c9308d97ca70b018ec13c
Moritz Spranger <moritz.spranger@gmail.com>
2021-11-04 11:23:30 +0100

    gh-pages

---
+++ precache-manifest.3b5b27b7b732a00bc362b29778fb2acd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6608227db52b209d78ba",
+    "url": "/making-history-poc/css/app.ea803a81.css"
+  },
+  {
+    "revision": "8cc9ffbd6367a9b7af13",
+    "url": "/making-history-poc/css/chunk-vendors.f19d484c.css"
+  },
+  {
+    "revision": "0dfa1c12d94b7bad857f",
+    "url": "/making-history-poc/css/home.34a9ae5b.css"
+  },
+  {
+    "revision": "7553da396e67deb3a8e4bbfc8db18c76",
+    "url": "/making-history-poc/img/antique.7553da39.jpg"
+  },
+  {
+    "revision": "95fcf97fe16e68c28d7512312a9a51e8",
+    "url": "/making-history-poc/img/aztecs.95fcf97f.jpg"
+  },
+  {
+    "revision": "a1453e89d10ff4af044fad5f60dc7d0f",
+    "url": "/making-history-poc/img/cuba-crisis.a1453e89.jpg"
+  },
+  {
+    "revision": "dd48b79b2d1ed6f1808801b527eec2ef",
+    "url": "/making-history-poc/img/french-revolution.dd48b79b.jpg"
+  },
+  {
+    "revision": "08d36a634945df4f135c8228e9612823",
+    "url": "/making-history-poc/img/german-reich.08d36a63.jpg"
+  },
+  {
+    "revision": "93643b0e42dc9b06149b999f1c30b253",
+    "url": "/making-history-poc/img/title-german.93643b0e.jpg"
+  },
+  {
+    "revision": "6ce6777ae897336d9d9aced32ffb0e4e",
+    "url": "/making-history-poc/img/title-history.6ce6777a.jpg"
+  },
+  {
+    "revision": "d584120c8ed1b98a738a746f4663d21f",
+    "url": "/making-history-poc/img/title-math.d584120c.jpg"
+  },
+  {
+    "revision": "bbb9463076e9557e39aebf0402914fdb",
+    "url": "/making-history-poc/index.html"
+  },
+  {
+    "revision": "6608227db52b209d78ba",
+    "url": "/making-history-poc/js/app.bb3a8223.js"
+  },
+  {
+    "revision": "8ceda2237b266bfea3ec",
+    "url": "/making-history-poc/js/chunk-2d216214.9ebff182.js"
+  },
+  {
+    "revision": "ac506c3460aa92a2c5b5",
+    "url": "/making-history-poc/js/chunk-2d216257.256ee085.js"
+  },
+  {
+    "revision": "5b5fca75ee9a9f2e5a1e",
+    "url": "/making-history-poc/js/chunk-3347db04.6f4d5dfe.js"
+  },
+  {
+    "revision": "8cc9ffbd6367a9b7af13",
+    "url": "/making-history-poc/js/chunk-vendors.d70916e2.js"
+  },
+  {
+    "revision": "0dfa1c12d94b7bad857f",
+    "url": "/making-history-poc/js/home.cd0e7a2e.js"
+  },
+  {
+    "revision": "c6e70317d2d3be652d24",
+    "url": "/making-history-poc/js/settings.cf29e1d7.js"
+  },
+  {
+    "revision": "33005d7895306831b6fb2ced6cd77c8f",
+    "url": "/making-history-poc/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/making-history-poc/robots.txt"
+  }
+]);
